0e93caa38801367c55b48d3aed30c61cf4d3625e
bashforger <bash.forger@gmail.com>
2020-11-18 19:30:41 +0500

    Fixed linters errors

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,16 +1,18 @@
 def bubble_sort(arr)
   swaps = 0
   (0..(arr.length - 2)).each do
-    j = 2; swaps = 0
+    j = 2
+    swaps = 0
     (0..(arr.length - j)).each do |i|
       if arr[i] > arr[i + 1]
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         swaps += 1
       end
-      j -= 1;
-    end # inner loop
-    break if swaps == 0
+      j -= 1
+    end
+    break if swaps.zero?
   end
+  arr
 end
 
-bubble_sort([1, 4, 6, 2, 3, 9, 7, 5])
+puts bubble_sort([1, 4, 6, 2, 3, 9, 7, 5]).inspect
