f5e5801788b05e8859e377dc414412b70264cb3a
bashforger <bash.forger@gmail.com>
2020-11-18 19:21:09 +0500

    Optimised Code

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,12 +1,20 @@
 def bubble_sort(arr)
-  (0..(arr.length-2)).each do 
-    (0..(arr.length-2)).each do |i|
-      if arr[i]>arr[i+1]
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+  swaps = 0
+  # iteration= 0
+  (0..(arr.length - 2)).each do
+    # iteration+=1
+    j = 2; swaps = 0
+    (0..(arr.length - j)).each do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swaps += 1
       end
-    end
+      j -= 1;
+    end # inner loop
+    break if swaps == 0
   end
-  print arr
+  # print "Array #{arr} Swaps #{swaps} Iterations #{iteration}"
 end
 
-bubble_sort([1,4,6,2,3,9,7,5])
+bubble_sort([1, 4, 6, 2, 3, 9, 7, 5])
+# bubble_sort([1,4,6,8,12,14,15,16])
